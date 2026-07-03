e09ffc576203859a9c635321b9f870c466279bb0
MystycCheez <golf_man_25@hotmail.com>
2026-07-03 17:30:45 -0400

    Replaced action handling with hashing

--- include/aoi.h
+++ include/aoi.h
@@ -23,17 +23,17 @@ typedef struct DA {
     unsigned long capacity;
 } DA;
 
-typedef struct UserEntry {
-    char* key;
+typedef struct HashEntry {
+    const char* key;
     void* value;
-} UserEntry;
+} HashEntry;
 
-typedef struct UD {
-    UserEntry* items;
+typedef struct HashData {
+    HashEntry* items;
     unsigned long count;
     unsigned long capacity;
     unsigned long hash;
-} UD;
+} HashData;
 
 typedef struct aoiData aoiData;
 
@@ -45,13 +45,17 @@ typedef struct Action {
     const char* desc;
 } Action;
 
-typedef struct ActionInfo {
-    int key;
-    int modifier;
-    int mouseButton;
-    int mouseDrag;
-    int scope;
-} ActionInfo;
+typedef union ActionBinding {
+    struct {
+        char key;
+        char modifier;
+        char mouseButton;
+        char mouseDrag;
+        char scope;
+    };
+    const char* binding;
+    char binding_a[5];
+} ActionBinding;
 
 typedef enum LogLevels {
     LOG_DEFAULT,
@@ -60,39 +64,48 @@ typedef enum LogLevels {
 
 typedef struct aoiData {
     LogLevels LogLevel;
-    ActionInfo InfoCounts;
-    ActionInfo ActionData;
-    DA RegisteredActions;
-    UD UserData;
-    Action** ActionTable;
+    ActionBinding ActiveBindings;
+    HashData ActionData;
+    HashData UserData;
+    unsigned long(*ActionHashFunction)(const ActionBinding, unsigned long hash);
+    unsigned long(*UserDataHashFunction)(char* name, unsigned long hash);
 } aoiData;
 
-Action* newAction(aoiData* Data, void (action)(aoiData*), const char* name, const char* desc);
-Action* newActionFromStruct(aoiData* Data, Action ActionDef);
-unsigned int getIndexFromStruct(aoiData* Data, struct ActionInfo ActionData);
-void addActionWithStruct(aoiData* Data, Action* action, struct ActionInfo ActionData);
-Action* getActionFromStruct(aoiData* Data, struct ActionInfo ActionData);
-Action* getActionFromName(aoiData* Data, const char* str);
-void setActionWithStruct(aoiData* Data, Action* action, struct ActionInfo ActionData);
+void InitActionData(aoiData* Data, unsigned long capacity);
+void SetActionHashFunction(aoiData* Data, unsigned long(*hash_fn)(const ActionBinding, unsigned long hash));
+Action* NewAction(void (action)(aoiData*), const char* name, const char* desc);
+void AddActionWithStruct(aoiData* Data, Action* action, ActionBinding Bindings);
+Action* GetActionFromStruct(aoiData* Data, ActionBinding Bindings);
+Action* GetActionFromName(aoiData* Data, const char* name);
+void SetActionWithStruct(aoiData* Data, Action* action, ActionBinding Bindings);
 
-aoiData* aoi_InitWithStruct(ActionInfo Maxes);
-void ActionHandlerFromStruct(aoiData* Data, ActionInfo ActionData);
+void ActionHandlerFromStruct(aoiData* Data, ActionBinding Bindings);
 void ActionHandler(aoiData* Data);
 
 void InitUserData(aoiData* Data, unsigned long capacity, unsigned long initHash);
-void AddUserData(aoiData* Data, char* name, void* data, unsigned long(*hash_fn)(char* name, unsigned long hash));
-void* GetUserData(aoiData* Data, char* name, unsigned long(*hash_fn)(char* name, unsigned long hash));
+void SetUserDataHashFunction(aoiData* Data, unsigned long(*hash_fn)(char* name, unsigned long hash));
+void AddUserData(aoiData* Data, char* name, void* data);
+void* GetUserData(aoiData* Data, char* name);
+
+aoiData* aoiInit(unsigned long initHash, unsigned long UD_capacity, unsigned long AD_capacity);
+void aoiCleanup(aoiData* Data);
 
 // Taken from https://x.com/vkrajacic/status/1749816169736073295
 #define aoi_Init(...) \
     _Pragma("GCC diagnostic push") \
     _Pragma("GCC diagnostic ignored \"-Winitializer-overrides\"") \
-    aoi_InitWithStruct((ActionInfo){.key = 260, .modifier = 8, .mouseButton = 8, .scope = 2, .mouseDrag = 2, __VA_ARGS__}) \
+    aoi_InitWithStruct((ActionBinding){.key = 255, .modifier = 8, .mouseButton = 8, .scope = 2, .mouseDrag = 2, __VA_ARGS__}) \
     _Pragma("GCC diagnostic pop") 
 
 
 #define addAction(aoiData, Action, ...) \
-    addActionWithStruct((aoiData), (Action), (ActionInfo){__VA_ARGS__})
+    addActionWithStruct((aoiData), (Action), (ActionBinding){__VA_ARGS__})
+
+#define AddAction(aoiData, Action, ...)\
+    AddActionWithStruct((aoiData), (Action), (ActionBinding){__VA_ARGS__})
+
+#define SetAction(aoiData, Action, ...)\
+    SetActionWithStruct((aoiData), (Action), (ActionBinding){__VA_ARGS__})
 
 // Keeping for reference
 // #define setUserData(aoiData, type, UD, value); \
--- src/aoi.c
+++ src/aoi.c
@@ -4,214 +4,214 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Default actions
-
-static void A_DoNothing(aoiData* Data)
-{
-    (void)Data;
-    return;
-}
-
-static void A_Exit(aoiData* Data)
-{
-    (void)Data;
-    exit(EXIT_SUCCESS);
-}
-
 // Action creation and modification functions
 
-// Returns a pointer to the newly created action
-Action* newAction(aoiData* Data, void (action)(aoiData*), const char* name, const char* desc)
+void InitActionData(aoiData* Data, unsigned long capacity)
 {
-    Action tmp = {.action = action, .name = name, .desc = desc};
-    Action* a_ptr = malloc(sizeof(*a_ptr));
-
-    if (a_ptr == NULL) {
-        fprintf(stderr, "newAction\n");
-        fprintf(stderr, "malloc\n");
-        exit(EXIT_FAILURE);
+    Data->ActionData.hash = 0; 
+    Data->ActionData.count = 0;
+    Data->ActionData.capacity = capacity;
+    Data->ActionData.items = malloc(sizeof(HashEntry) * capacity);
+    for (size_t i = 0; i < capacity; i++) {
+        Data->ActionData.items[i].value = NULL;
+        Data->ActionData.items[i].key = NULL;
     }
+}
 
-    memcpy(a_ptr, &tmp, sizeof(*a_ptr));
+void SetActionHashFunction(aoiData* Data, unsigned long(*hash_fn)(const ActionBinding, unsigned long hash))
+{
+    Data->ActionHashFunction = hash_fn;
+}
 
-    da_append(Data->RegisteredActions, a_ptr);
-    
-    if (Data->RegisteredActions.items[Data->RegisteredActions.count - 1] == NULL) {
-        fprintf(stderr, "newAction\n");
-        fprintf(stderr, "da_append\n");
-        exit(EXIT_FAILURE);
+Action* NewAction(void (action)(aoiData*), const char* name, const char* desc)
+{
+    Action* a = malloc(sizeof(Action));
+    Action tmp = {action, name, desc};
+    memcpy(a, &tmp, sizeof(Action));
+
+    return a;
+}
+
+void ResizeActionHashTable(aoiData* Data)
+{
+    HashEntry* tmp = malloc(sizeof(HashEntry) * Data->ActionData.capacity * 1.5);
+    for (size_t i = 0; i < Data->ActionData.capacity; i++) tmp[i].value = NULL;
+
+    unsigned long index;
+    for (size_t i = 0; i < Data->ActionData.capacity; i++) {
+        if (!Data->ActionData.items[i].key) continue;
+        unsigned long hash = Data->ActionHashFunction((ActionBinding)Data->ActionData.items[i].key, Data->ActionData.hash);
+        index = hash % Data->ActionData.capacity;
+        tmp[index].key = Data->ActionData.items[i].key;
+        tmp[index].value = Data->ActionData.items[i].value;
     }
-    return a_ptr;
+    Data->ActionData.capacity *= 1.5;
+    Data->ActionData.items = realloc(Data->ActionData.items, sizeof(HashEntry) * Data->ActionData.capacity);
+    mempcpy(Data->ActionData.items, tmp, Data->ActionData.capacity);
+    free(tmp);
 }
 
-Action* newActionFromStruct(aoiData* Data, Action ActionDef)
+void ResizeUserDataHashTable(aoiData* Data)
 {
-    return newAction(Data, ActionDef.action, ActionDef.name, ActionDef.desc);
-}
+    HashEntry* tmp = malloc(sizeof(HashEntry) * Data->UserData.capacity * 1.5);
+    for (size_t i = 0; i < Data->UserData.capacity; i++) tmp[i].value = NULL;
 
-unsigned int getIndexFromStruct(aoiData* Data, ActionInfo ActionData)
-{
-    // v * (W * X * Y * Z) + w * (X * Y * Z) + x * (Y * Z) + y * Z + z
-
-    // This shouldn't happen, but I'll keep it here incase it does
-    if ((ActionData.key < 0) ||
-        (ActionData.modifier < 0) ||
-        (ActionData.mouseButton < 0) ||
-        (ActionData.mouseDrag < 0) ||
-        (ActionData.scope < 0)
-    ) return 0;
-
-    unsigned int index = 0;
-    
-    int keyCount = Data->InfoCounts.key;
-    int modifierCount = Data->InfoCounts.modifier;
-    int mouseButtonCount = Data->InfoCounts.mouseButton;
-    int isMoving = Data->InfoCounts.mouseDrag;
-
-    index = (unsigned int){
-        (ActionData.scope * (keyCount * modifierCount * mouseButtonCount * isMoving)) +
-        (ActionData.key * (modifierCount * mouseButtonCount * isMoving)) +
-        (ActionData.modifier * (mouseButtonCount * isMoving)) +
-        (ActionData.mouseButton * isMoving) +
-        (ActionData.mouseDrag)
-    };
-    #ifdef AOI_DEBUG
-    printf("%d\n", ActionData.key);
-    printf("%d\n", ActionData.modifier);
-    printf("%d\n", ActionData.mouseButton);
-    printf("%d\n", ActionData.mouseDrag);
-    printf("%d\n", ActionData.scope);
-    #endif
-    return index;
-}
-
-void addActionWithStruct(aoiData* Data, Action* action, ActionInfo ActionData)
-{
-    unsigned int index = getIndexFromStruct(Data, ActionData);
-    #ifdef AOI_DEBUG
-    printf("%s: %u\n", action->name, index);
-    #endif
-    Data->ActionTable[index] = action;
-}
-
-Action* getActionFromStruct(aoiData* Data, ActionInfo ActionData)
-{
-    unsigned int index = getIndexFromStruct(Data, ActionData);
-    #ifdef AOI_DEBUG
-    printf("Index: %u\n", index);
-    #endif
-
-    Action* action = Data->ActionTable[index];
-    if (action == NULL) {
-        return Data->ActionTable[0];
+    unsigned long index;
+    for (size_t i = 0; i < Data->UserData.capacity; i++) {
+        if (!Data->UserData.items[i].key) continue;
+        unsigned long hash = Data->UserDataHashFunction((char*)Data->UserData.items[i].key, Data->UserData.hash);
+        index = hash % Data->UserData.capacity;
+        tmp[index].key = Data->UserData.items[i].key;
+        tmp[index].value = Data->UserData.items[i].value;
     }
-    return action;
+    Data->UserData.capacity *= 1.5;
+    Data->UserData.items = realloc(Data->UserData.items, sizeof(HashEntry) * Data->UserData.capacity);
+    mempcpy(Data->UserData.items, tmp, Data->UserData.capacity);
+    free(tmp);
 }
 
-Action* getActionFromName(aoiData* Data, const char* str)
+void AddActionWithStruct(aoiData* Data, Action* action, ActionBinding Bindings)
 {
-    unsigned int index = 0;
-    
-    DA* RegisteredActions = &Data->RegisteredActions;
-    Action* a_ptr = RegisteredActions->items[index];
+    if (Data->ActionData.count > (Data->ActionData.capacity * 0.75)) ResizeActionHashTable(Data);
 
-    while (a_ptr != NULL) {
-        // printf("%s\n", a_ptr->name);
-        if (strcmp(a_ptr->name, str) == 0) return a_ptr;
-        a_ptr = RegisteredActions->items[++index];
+    unsigned long hash = Data->ActionHashFunction(Bindings, Data->ActionData.hash);
+    unsigned long index = hash % Data->UserData.capacity;
+    Data->ActionData.items[index].key = Bindings.binding;
+    Data->ActionData.items[index].value = action;
+
+    Data->ActionData.count++;
+}
+
+void SetActionWithStruct(aoiData* Data, Action* action, ActionBinding Bindings)
+{
+    unsigned long hash = Data->ActionHashFunction(Bindings, Data->ActionData.hash);
+    unsigned long index = hash % Data->UserData.capacity;
+    Data->ActionData.items[index].value = action;   
+}
+
+Action* GetActionFromStruct(aoiData* Data, ActionBinding Bindings)
+{
+    unsigned long hash = Data->ActionHashFunction(Bindings, Data->ActionData.hash);
+    unsigned long index = hash % Data->UserData.capacity;
+    printf("%s\n", ((Action*)Data->ActionData.items[index].value)->name);
+    return Data->ActionData.items[index].value;
+}
+
+Action* GetActionFromName(aoiData* Data, const char* name)
+{
+    for (size_t i = 0; i < Data->ActionData.count; i++) {
+        if (strcmp(((Action*)Data->ActionData.items[i].value)->name, name)) return Data->ActionData.items[i].value;
     }
     fprintf(stderr, "Action not found\n");
-    fprintf(stderr, "Returning \"Do Nothing\"\n");
-    return RegisteredActions->items[0];
-}
-
-void setActionWithStruct(aoiData* Data, Action* action, ActionInfo ActionData)
-{
-    int index = getIndexFromStruct(Data, ActionData);
-    Data->ActionTable[index] = action;
+    return NULL;
 }
 
 //
 
-aoiData* aoi_InitWithStruct(ActionInfo Maxes)
+void ActionHandlerFromStruct(aoiData* Data, ActionBinding Bindings)
 {
-    aoiData* Data = malloc(sizeof(aoiData));
-
-    Data->LogLevel = LOG_DEFAULT;
-
-    Data->RegisteredActions.count = 0;
-    Data->RegisteredActions.capacity = 4;
-    Data->RegisteredActions.items = malloc(sizeof(Action*) * Data->RegisteredActions.capacity);
-
-    // Data->InfoCounts = (ActionInfo){.key = 260, .modifier = 8, .mouseButton = 8, .scope = 2, .mouseDrag = 2};
-    unsigned int size = Maxes.key * Maxes.modifier * Maxes.mouseButton * Maxes.mouseDrag * Maxes.scope;
-    Data->InfoCounts = Maxes;
-
-    
-    Data->ActionData = (ActionInfo){0};
-
-    Data->ActionTable = calloc(sizeof(Action*), size);
-    if (Data->ActionTable == NULL) {
-        fprintf(stderr, "ActionTable malloc\n");
-        exit(EXIT_FAILURE);
-    }
-
-    Action* DO_NOTHING = newAction(
-        Data,
-        A_DoNothing,
-        "Do Nothing",
-        "Do nothing"
-    );
-    addAction(Data, DO_NOTHING);
-
-    newAction(
-        Data,
-        A_Exit,
-        "Exit",
-        "Exit program"
-    );
-
-    return Data;
-}
-
-void ActionHandlerFromStruct(aoiData* Data, ActionInfo ActionData)
-{
-    Action* action = getActionFromStruct(Data, ActionData);
-    action->action(Data);
+    GetActionFromStruct(Data, Bindings)->action(Data);
 }
 
 void ActionHandler(aoiData* Data)
 {
-    ActionHandlerFromStruct(Data, Data->ActionData);
+    ActionHandlerFromStruct(Data, Data->ActiveBindings);
 }
 
 // User Data
 
 void InitUserData(aoiData* Data, unsigned long capacity, unsigned long initHash)
 {
-    if (capacity <= 0) {
+    if (!capacity) {
         fprintf(stderr, "Capacity must be greater than 0!\n");
         exit(EXIT_FAILURE);
     }
     Data->UserData.capacity = capacity;
     Data->UserData.count = 0;
-    Data->UserData.items = malloc(sizeof(UserEntry) * capacity);
+    Data->UserData.items = malloc(sizeof(HashEntry) * capacity);
     Data->UserData.hash = initHash;
+    for (size_t i = 0; i < capacity; i++) {
+        Data->UserData.items[i].value = NULL;
+        Data->UserData.items[i].key = NULL;
+    }
 }
 
-void AddUserData(aoiData* Data, char* name, void* data, unsigned long(*hash_fn)(char* name, unsigned long hash))
+void SetUserDataHashFunction(aoiData* Data, unsigned long(*hash_fn)(char* name, unsigned long hash))
 {
-    unsigned long hash = hash_fn(name, Data->UserData.hash);
+    Data->UserDataHashFunction = hash_fn;
+}
+
+void AddUserData(aoiData* Data, char* name, void* data)
+{
+    if (Data->UserData.count > (Data->UserData.capacity * 0.75)) {
+        Data->UserData.capacity *= 1.5;
+        Data->UserData.items = realloc(Data->UserData.items, sizeof(HashEntry) * Data->UserData.capacity);
+    }
+
+    unsigned long hash = Data->UserDataHashFunction(name, Data->UserData.hash);
     unsigned long index = hash % Data->UserData.capacity;
-    // printf("AddUserData: index: %ld\n", index);
     Data->UserData.items[index].key = strdup(name);
     Data->UserData.items[index].value = data;
+    Data->UserData.count++;
 }
 
-void* GetUserData(aoiData* Data, char* name, unsigned long(*hash_fn)(char* name, unsigned long hash))
+void* GetUserData(aoiData* Data, char* name)
 {
-    unsigned long hash = hash_fn(name, Data->UserData.hash);
+    unsigned long hash = Data->UserDataHashFunction(name, Data->UserData.hash);
     unsigned long index = hash % Data->UserData.capacity;
-    // printf("GetUserData: index: %ld\n", index);
     return Data->UserData.items[index].value;
+}
+
+//
+
+aoiData* aoiInit(unsigned long initHash, unsigned long UD_capacity, unsigned long AD_capacity)
+{
+    aoiData* Data = malloc(sizeof(aoiData));
+    InitActionData(Data, AD_capacity);
+    InitUserData(Data, UD_capacity, initHash);
+
+    // Why did I add LogLevel???????????????
+    Data->LogLevel = LOG_DEFAULT;
+    
+    Data->ActiveBindings = (ActionBinding){0};
+
+    return Data;
+}
+
+void UserDataCleanup(aoiData* Data)
+{
+    size_t index = 0;
+    void* tmp = Data->UserData.items[index].value;
+    while (index < Data->UserData.capacity) {
+        if (tmp == Data->UserData.items[index].value) {index++; continue;}
+        if (Data->UserData.items[index].value) {
+            printf("UserData[%zu]: %p\n", index, Data->UserData.items[index].value);
+            free(Data->UserData.items[index].value);
+        }
+        index++;
+    }
+}
+
+void ActionDataCleanup(aoiData* Data)\
+{
+    size_t index = 0;
+    void* tmp = Data->ActionData.items[index].value;
+    while (index < Data->ActionData.capacity) {
+        if (tmp == Data->ActionData.items[index].value) {index++; continue;}
+        if (Data->ActionData.items[index].value) {
+            printf("ActionData[%zu]: %p\n", index, Data->ActionData.items[index].value);
+            free(Data->ActionData.items[index].value);
+        }
+        index++;
+    }
+}
+
+void aoiCleanup(aoiData* Data)
+{
+    UserDataCleanup(Data);
+    ActionDataCleanup(Data);
+
+    free(Data->UserData.items);
+    free(Data->ActionData.items);
+    free(Data);
 }
